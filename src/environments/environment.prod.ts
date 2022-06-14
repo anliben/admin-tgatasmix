b91d52bd5794a8942e05d480b2fdfe4a91b4bb42
João Victor <45929971+anliben@users.noreply.github.com>
2022-06-14 18:55:44 -0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,14 @@
 export const environment = {
   production: true,
-  api: 'http://app-37104.nuvem-us-02.absamcloud.com'
+  api: '',
+   firebaseConfig: {
+    apiKey: "AIzaSyDjuU60FLg02ARUFjdDEbrDWRCI-dPGIOA",
+    authDomain: "tgatasmix-ba392.firebaseapp.com",
+    databaseURL: "https://tgatasmix-ba392-default-rtdb.firebaseio.com",
+    projectId: "tgatasmix-ba392",
+    storageBucket: "tgatasmix-ba392.appspot.com",
+    messagingSenderId: "982192088583",
+    appId: "1:982192088583:web:9770c1ab1a9c97d1e52ca2",
+    measurementId: "G-8Z3QCHYVLG"
+  }
 };
